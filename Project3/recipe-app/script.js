b3c8d81bb1d04ddd8d4d632e32ac6d2dd0803575
zehra <zehrakocairi@gmail.com>
2021-11-27 22:57:53 +0000

    Project-3 did not complete

--- Project3/recipe-app/script.js
+++ Project3/recipe-app/script.js
@@ -1,45 +1,33 @@
-let getCategories = async function () {
-    var response = await fetch("https://www.themealdb.com/api/json/v1/1/categories.php");
-    var categories = await response.json();
+let getMealData = async function () {
+  let resp = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/categories.php"
+  );
+  let meals = await resp.json();
 
-    return categories;
-}
-
-// async function test(){
-
-//     await getCategories();
-// }
-var categories;
-getCategories().then((data) => {
-    categories = data;
-    localStorage.setItem("car", "araba");
-    localStorage.setItem("categories", JSON.stringify(categories) );
+  return meals;
+};
+let meals;
+getMealData().then((data) => {
+  meals = data;
+  localStorage.setItem("meals", JSON.stringify(meals));
 });
 
-let getRandom = async function () {
-    var meaning = localStorage.getItem("car");
-    var categoriler = JSON.parse(localStorage.getItem("categories"));
-    debugger;
-    var response = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
-    return await response.json();
-}
-// getRandom().then(({meals})=>{
-//     debugger;
-// });
-function fetchRandomReceipt() {
-    getRandom().then((data) => {
-        let { strMealThumb, strMeal } = data.meals[0];
-        let reciptDom = document.getElementById("rondom-recipe");
-        let zehralll = `<p id="up-text">Recipe of the day</p>
-                        <img
-                        src="${strMealThumb}"
-                        alt="${strMeal}"
-                        />
-                        <div id="meal-desc">
-                        <h5>${strMeal}</h5>
-                        </div>`
-            ;
-        reciptDom.innerHTML = zehralll;
-    });
-}
+let getRandomMeal = async function () {
+  let resp = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+  return await resp.json();
+};
 
+let randomMeal;
+let addRandomMeals = function () {
+  getRandomMeal().then((data) => {
+    randomMeal = data.meals[0];
+  });
+  let randomRecipes = document.getElementById("random-recipe");
+  randomRecipes.innerHTML = `<div id="random-recipe">
+        <p id="up-text">Recipe of the day</p>
+        <img src="${randomMeal.strMealThumb}" alt="" />
+        <div id="meal-desc">
+          <h5>${randomMeal.strMeal}</h5>
+        </div>
+      </div>`;
+};
