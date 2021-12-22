ff03eac7fe78e29033219f5624de985feef79edf
Zekeriya <zekeriyakocairi@gmail.com>
2021-12-22 20:04:58 +0100

    Some styling

--- Project6/movie-app/script.js
+++ Project6/movie-app/script.js
@@ -1,13 +1,9 @@
-const APIURL =
-  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCHAPI =
-  "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
-let main = document.getElementById("main");
-let imdb = document.getElementsByClassName("imdb");
+let mainDiv = document.getElementById("main");
 let form = document.getElementById("form");
-let search = document.getElementById("search");
 
 getMovies(APIURL);
 
@@ -19,12 +15,12 @@ async function getMovies(url) {
 }
 
 function showMovies(movies) {
-  main.innerHTML = "";
+  mainDiv.innerHTML = "";
   movies.forEach((movie) => {
     const { poster_path, title, vote_average, overview } = movie;
-    let newDiv = document.createElement("div");
-    newDiv.classList.add("movies");
-    newDiv.innerHTML = `
+    let movieDiv = document.createElement("div");
+    movieDiv.classList.add("movies");
+    movieDiv.innerHTML = `
     <div  class="movie">
         <img
         src="${IMGPATH + poster_path}"
@@ -41,19 +37,18 @@ function showMovies(movies) {
     </div>
     
    `;
-    main.appendChild(newDiv);
+    mainDiv.appendChild(movieDiv);
   });
 }
 
-function colorImdb(value) {
-  return value >= 8 ? "green" : value >= 5 ? "orange" : "red";
-}
+let colorImdb = (value) => value >= 8 ? "green" : value >= 5 ? "orange" : "red";
 
 form.addEventListener("submit", (e) => {
+let searchInput = document.getElementById("search");
   e.preventDefault();
-  let searchValue = search.value;
+  let searchValue = searchInput.value;
   if (searchValue) {
     getMovies(SEARCHAPI + searchValue);
-    search.value = "";
+    searchInput.value = "";
   }
 });
