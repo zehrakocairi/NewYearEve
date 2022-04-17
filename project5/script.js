4c405f7c6557acaaf08fcb552fb93be5860f2897
zehra <zehrakocairi@gmail.com>
2022-04-17 10:20:11 +0100

    almost completed

--- project5/script.js
+++ project5/script.js
@@ -1,31 +1,72 @@
-let form = document.querySelector("form");
-let input = document.querySelector("input");
-let todos = document.querySelector(".todos");
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  addList();
+});
+
+const toDos = JSON.parse(localStorage.getItem("allToDos"));
+
+if (toDos) {
+  toDos.forEach((toDo) => {
+    addList(toDo);
+  });
+}
 
 function lineThrough(e) {
-  const li = e.target.parentNode.parentNode.querySelector("ul li");
-  li.classList.toggle("done");
+  const li = e.target.parentNode.parentNode.querySelector(".toDos .toDo");
+  li.classList.toggle("completed");
+
+  updateLS();
 }
 
 function remove(e) {
   e.target.parentNode.parentNode.remove();
+
+  updateLS();
 }
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  let todoText = input.value;
-  if (todoText) {
-    let cover = document.createElement("div");
-    cover.classList.add("cover");
-    cover.innerHTML = `
-    <ul class="todos">
-      <li>${todoText}</li>
+function addList(toDo) {
+  let toDoText = input.value;
+
+  if (toDo) {
+    toDoText = toDo.text;
+  }
+
+  if (toDoText) {
+    let lists = document.createElement("div");
+
+    lists.classList.add("list");
+    lists.innerHTML = `
+    <ul class="toDos">
+     <li class="toDo">${toDoText}</li>
     </ul>
     <div class="buttonEls">
-      <button id="done" class="" onclick="lineThrough(event)">✔</button>
-      <button id="remove" class="" onclick="remove(event)">✖</button>
-    </div>`;
-    form.appendChild(cover);
+     <button class="done" onclick="lineThrough(event)">✔</button>
+     <button class="remove" onclick="remove(event)">✖</button>
+    </div>
+   `;
+
+    form.appendChild(lists);
   }
+
   input.value = "";
-});
+  updateLS();
+}
+
+function updateLS() {
+  const toDosEl = document.querySelectorAll("li");
+
+  const allToDos = [];
+
+  toDosEl.forEach((toDo) => {
+    allToDos.push({
+      text: toDo.innerText,
+      completed: toDo.classList.contains("completed"),
+    });
+  });
+
+  localStorage.setItem("allToDos", JSON.stringify(allToDos));
+}
